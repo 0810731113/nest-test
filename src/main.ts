0c3526b8a8babe27d20a83ee3703089140565da4
libo <431397516@qq.com>
2021-01-26 23:52:55 +0800

    测试末班

--- src/main.ts
+++ src/main.ts
@@ -5,31 +5,36 @@ import {getHost} from './utils/util';
 import {ConfigService} from '@nestjs/config';
 import {createProxyMiddleware} from 'http-proxy-middleware';
 import {logger} from './middleware/logger.middleware';
-// const cookieParser = require('cookie-parser') ;
-// const bodyParser = require('body-parser');
-// import * as express from 'express';
+const cookieParser = require('cookie-parser') ;
+const bodyParser = require('body-parser');
+import * as express from 'express';
 import {TransformInterceptor} from './interceptor/transform.interceptor';
 import {HttpExceptionFilter} from './filter/http-exception.filter';
 import {AllExceptionsFilter} from './filter/any-exception.filter';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-// import { NestExpressApplication } from '@nestjs/platform-express';
-// import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+// import path,{ join } from 'path';
+import * as path from 'path';
 
 async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const app = await NestFactory.create(AppModule);
-  // app.use(express.json()); // For parsing application/json
-  // app.use(express.urlencoded({ extended: true }));
-  // app.use(cookieParser());
-  // app.use(bodyParser.urlencoded({ extended: false }))
-  app.useGlobalPipes(new ValidationPipe())
-  app.useGlobalInterceptors(new TransformInterceptor());
-  app.useGlobalFilters(new AllExceptionsFilter())
-  app.useGlobalFilters(new HttpExceptionFilter())
-  // app.useStaticAssets(join(__dirname, '..', 'public'),{
-  //   prefix: '/static/',   //设置虚拟路径
-  // });
-
+  app.use(express.json()); // For parsing application/json
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  app.use(bodyParser.urlencoded({ extended: false }))
+  // app.useGlobalPipes(new ValidationPipe())
+  // app.useGlobalInterceptors(new TransformInterceptor());
+  // app.useGlobalFilters(new AllExceptionsFilter())
+  // app.useGlobalFilters(new HttpExceptionFilter())
+  // app.useStaticAssets(path.join('public'));
+  // app.useStaticAssets(path.join('public'));
+  app.useStaticAssets(path.join('public'),{
+    prefix: '/assets/',   //设置虚拟路径
+  });
+  app.setBaseViewsDir(path.join('views'));
+  // app.setBaseViewsDir(path.join(__dirname, 'views'));
+  app.setViewEngine('hbs');
   const configService = app.get(ConfigService);
   console.log(`-------------configService--------------`);
   console.log(configService);
